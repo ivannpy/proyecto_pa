5b634ecddbbd14724140595fc230d036fb219228
Ivannpy <jivanreyesh@gmail.com>
2025-11-19 15:40:29 -0600

    Menciona uso del patron Factory en la fabrica de procesadores

--- proyecto/src/main/java/unam/pcic/FabricaProcesador.java
+++ proyecto/src/main/java/unam/pcic/FabricaProcesador.java
@@ -1,7 +1,7 @@
 package unam.pcic;
 
 /**
- * - Crea instancias de ProcesadorCSV según sea secuencial o concurrente.
+ * - Crea instancias de ProcesadorCSV según sea secuencial o concurrente usando el patrón Factory.
  * - Inyecta dependencias.
  */
 public class FabricaProcesador {
