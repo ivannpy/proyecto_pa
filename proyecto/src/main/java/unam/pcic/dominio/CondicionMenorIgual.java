ff4eff6b82dcf3377abb4dff1fe4df91e63af2b0
Ivannpy <jivanreyesh@gmail.com>
2025-11-29 15:23:38 -0600

    Agrega condiciones de filtrado faltantes

---
+++ proyecto/src/main/java/unam/pcic/dominio/CondicionMenorIgual.java
@@ -0,0 +1,34 @@
+package unam.pcic.dominio;
+
+/**
+ * Implementa la condición de 'mayor que' de registro según el valor de una columna.
+ */
+public class CondicionMenorIgual implements CondicionFiltro<RegistroCSV> {
+    /** Columna del registro a comparar */
+    private final int columna;
+
+    /** Valor buscado */
+    private final String valor;
+
+    /**
+     * Constructor.
+     *      Representa una condición de filtrado.
+     *
+     * @param columna La columna del registro a comparar.
+     * @param valor El valor buscado.
+     */
+    public CondicionMenorIgual(int columna, String valor) {
+        this.columna = columna;
+        this.valor = valor;
+    }
+
+    /**
+     * Regresa true si el registro dado tiene el valor buscado en la columna indicada.
+     *
+     * @param registro El registro a comparar.
+     * @return true si el registro dado tiene el valor buscado en la columna indicada, false en caso contrario.
+     */
+    public boolean cumple(RegistroCSV registro) {
+        return registro.tieneValor(columna) && registro.getValor(columna).compareTo(valor) <= 0;
+    }
+}
